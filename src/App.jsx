c29385ab865fafd0de6f0348cf3a4abe32b6e34b
PeterAires <168139612+PeterAires@users.noreply.github.com>
2024-06-20 20:20:01 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const [calculadora,setCalculadora] = useState([])
+  const [primeiroNumero,setPrimeiroNumero] = useState()
+  const [operador,setOperador] = useState()
+  const [segundoNumero,setSegundoNumero] = useState()
+
+  
+
+  return(
+    <div>
+      <button onClick={(e) => setPrimeiroNumero(1)}>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>0</button>
+      <button>%</button>
+      <button>x</button>
+      <button>-</button>
+      <button>+</button>
+      <button>=</button>
+      {primeiroNumero}
+
+    </div>
+      )
 }
-
 export default App
